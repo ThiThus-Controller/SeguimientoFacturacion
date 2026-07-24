e4ffd53a4bfef762d659c5e004d1226eed96aca7
ThiThus-Controller <96jhernandez26@gmail.com>
2026-07-24 08:26:58 -0500

    refactor(architecture): corregir dependencias entre capas

--- SeguimientoFacturacion.Infrastructure/obj/Debug/net10.0/SeguimientoFacturacion.Infrastructure.AssemblyInfo.cs
+++ SeguimientoFacturacion.Infrastructure/obj/Debug/net10.0/SeguimientoFacturacion.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SeguimientoFacturacion.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49ac735b74e3ee60059b9eedf7a977fe556e4651")]
 [assembly: System.Reflection.AssemblyProductAttribute("SeguimientoFacturacion.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SeguimientoFacturacion.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
