c0c609076f3c39c86c57d7d384f0ffd25626a3d5
ThiThus-Controller <96jhernandez26@gmail.com>
2026-07-24 15:00:36 -0500

    feat(infrastructure): agregar migracion de movimientos anuales

---
+++ SeguimientoFacturacion.Infrastructure/Persistence/Migrations/20260724195327_PermitirMovimientosAnualesSinFecha.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SeguimientoFacturacion.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class PermitirMovimientosAnualesSinFecha :
+        Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(
+            MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Movimientos_FacturaId_Fecha",
+                schema: "facturacion",
+                table: "Movimientos");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "Fecha",
+                schema: "facturacion",
+                table: "Movimientos",
+                type: "date",
+                nullable: true,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+
+            /*
+                Primero se agrega Anio como nullable.
+
+                Esto permite trasladar de forma segura el año de
+                cualquier movimiento que pudiera existir antes de
+                convertir la columna en obligatoria.
+            */
+            migrationBuilder.AddColumn<int>(
+                name: "Anio",
+                schema: "facturacion",
+                table: "Movimientos",
+                type: "int",
+                nullable: true);
+
+            /*
+                En el modelo anterior Fecha era obligatoria.
+                Por eso es posible recuperar el año de los registros
+                existentes sin inventar información.
+            */
+            migrationBuilder.Sql(
+                """
+                UPDATE [facturacion].[Movimientos]
+                SET [Anio] = YEAR([Fecha])
+                WHERE [Anio] IS NULL;
+                """);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Anio",
+                schema: "facturacion",
+                table: "Movimientos",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name:
+                    "IX_Movimientos_FacturaId_Anio_Fecha",
+                schema: "facturacion",
+                table: "Movimientos",
+                columns:
+                    new[]
+                    {
+                        "FacturaId",
+                        "Anio",
+                        "Fecha"
+                    });
+
+            migrationBuilder.AddCheckConstraint(
+                name: "CK_Movimientos_Anio",
+                schema: "facturacion",
+                table: "Movimientos",
+                sql: "[Anio] BETWEEN 2000 AND 9999");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(
+            MigrationBuilder migrationBuilder)
+        {
+            /*
+                No se permite regresar al modelo anterior si existen
+                movimientos sin fecha, porque hacerlo obligaría a
+                inventar una fecha.
+            */
+            migrationBuilder.Sql(
+                """
+                IF EXISTS
+                (
+                    SELECT 1
+                    FROM [facturacion].[Movimientos]
+                    WHERE [Fecha] IS NULL
+                )
+                BEGIN
+                    THROW 51001,
+                        'No se puede revertir la migración porque existen movimientos sin fecha exacta.',
+                        1;
+                END;
+                """);
+
+            migrationBuilder.DropIndex(
+                name:
+                    "IX_Movimientos_FacturaId_Anio_Fecha",
+                schema: "facturacion",
+                table: "Movimientos");
+
+            migrationBuilder.DropCheckConstraint(
+                name: "CK_Movimientos_Anio",
+                schema: "facturacion",
+                table: "Movimientos");
+
+            migrationBuilder.DropColumn(
+                name: "Anio",
+                schema: "facturacion",
+                table: "Movimientos");
+
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "Fecha",
+                schema: "facturacion",
+                table: "Movimientos",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movimientos_FacturaId_Fecha",
+                schema: "facturacion",
+                table: "Movimientos",
+                columns:
+                    new[]
+                    {
+                        "FacturaId",
+                        "Fecha"
+                    });
+        }
+    }
+}
---
+++ database/scripts/004_PermitirMovimientosAnualesSinFecha.sql
@@ -0,0 +1,83 @@
+﻿BEGIN TRANSACTION;
+IF NOT EXISTS (
+    SELECT * FROM [__SeguimientoFacturacionMigrationsHistory]
+    WHERE [MigrationId] = N'20260724195327_PermitirMovimientosAnualesSinFecha'
+)
+BEGIN
+    DROP INDEX [IX_Movimientos_FacturaId_Fecha] ON [facturacion].[Movimientos];
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__SeguimientoFacturacionMigrationsHistory]
+    WHERE [MigrationId] = N'20260724195327_PermitirMovimientosAnualesSinFecha'
+)
+BEGIN
+    DECLARE @var nvarchar(max);
+    SELECT @var = QUOTENAME([d].[name])
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[facturacion].[Movimientos]') AND [c].[name] = N'Fecha');
+    IF @var IS NOT NULL EXEC(N'ALTER TABLE [facturacion].[Movimientos] DROP CONSTRAINT ' + @var + ';');
+    ALTER TABLE [facturacion].[Movimientos] ALTER COLUMN [Fecha] date NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__SeguimientoFacturacionMigrationsHistory]
+    WHERE [MigrationId] = N'20260724195327_PermitirMovimientosAnualesSinFecha'
+)
+BEGIN
+    ALTER TABLE [facturacion].[Movimientos] ADD [Anio] int NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__SeguimientoFacturacionMigrationsHistory]
+    WHERE [MigrationId] = N'20260724195327_PermitirMovimientosAnualesSinFecha'
+)
+BEGIN
+    UPDATE [facturacion].[Movimientos]
+    SET [Anio] = YEAR([Fecha])
+    WHERE [Anio] IS NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__SeguimientoFacturacionMigrationsHistory]
+    WHERE [MigrationId] = N'20260724195327_PermitirMovimientosAnualesSinFecha'
+)
+BEGIN
+    DECLARE @var1 nvarchar(max);
+    SELECT @var1 = QUOTENAME([d].[name])
+    FROM [sys].[default_constraints] [d]
+    INNER JOIN [sys].[columns] [c] ON [d].[parent_column_id] = [c].[column_id] AND [d].[parent_object_id] = [c].[object_id]
+    WHERE ([d].[parent_object_id] = OBJECT_ID(N'[facturacion].[Movimientos]') AND [c].[name] = N'Anio');
+    IF @var1 IS NOT NULL EXEC(N'ALTER TABLE [facturacion].[Movimientos] DROP CONSTRAINT ' + @var1 + ';');
+    ALTER TABLE [facturacion].[Movimientos] ALTER COLUMN [Anio] int NOT NULL;
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__SeguimientoFacturacionMigrationsHistory]
+    WHERE [MigrationId] = N'20260724195327_PermitirMovimientosAnualesSinFecha'
+)
+BEGIN
+    CREATE INDEX [IX_Movimientos_FacturaId_Anio_Fecha] ON [facturacion].[Movimientos] ([FacturaId], [Anio], [Fecha]);
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__SeguimientoFacturacionMigrationsHistory]
+    WHERE [MigrationId] = N'20260724195327_PermitirMovimientosAnualesSinFecha'
+)
+BEGIN
+    EXEC(N'ALTER TABLE [facturacion].[Movimientos] ADD CONSTRAINT [CK_Movimientos_Anio] CHECK ([Anio] BETWEEN 2000 AND 9999)');
+END;
+
+IF NOT EXISTS (
+    SELECT * FROM [__SeguimientoFacturacionMigrationsHistory]
+    WHERE [MigrationId] = N'20260724195327_PermitirMovimientosAnualesSinFecha'
+)
+BEGIN
+    INSERT INTO [__SeguimientoFacturacionMigrationsHistory] ([MigrationId], [ProductVersion])
+    VALUES (N'20260724195327_PermitirMovimientosAnualesSinFecha', N'10.0.10');
+END;
+
+COMMIT;
+GO
+
